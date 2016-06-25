b49a6732d2d3027af4752ef8c675490a5d48ca9b
filipecosta90 <filipecosta.90@gmail.com>
2016-06-25 17:51:46 +0100

    removed size conflit

--- MD_MPI/main.c
+++ MD_MPI/main.c
@@ -38,9 +38,8 @@ int main(int argc, char  *argv[]){
   GET_TIME(end);
   elapsed = end - start;
 
-  int rank, size;
+  int rank;
   MPI_Comm_rank (MPI_COMM_WORLD, &rank);	/* get current process id */
-  MPI_Comm_size (MPI_COMM_WORLD, &size);	/* get number of processes */
   if (rank == 0){
     printf( "%f\n", elapsed );
   }
